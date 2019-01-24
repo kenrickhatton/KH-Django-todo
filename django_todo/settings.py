233829e18785ded50e954cc6e76e3db34bbeeaee
Kenrick Hatton <kenrick.hatton@gmail.com>
2019-01-24 15:05:51 +0000

    changes to settings and requirments  database prep with heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,12 +75,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
+DATABASES = {'default': dj_database_url.parse("postgres://dzcrzxprcczzug:ce92947036bfc63a313d8511ad450ea18d5404b240fec445f5dd088c0b265cc5@ec2-54-228-224-37.eu-west-1.compute.amazonaws.com:5432/da4vl0pg3mbpam")}
 
 
 # Password validation
